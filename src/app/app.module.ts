5d1598d05afdb6b042aacc91e9cccba056681fb3
viktoria <vik.slepchenko@gmail.com>
2019-12-13 22:17:16 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { SelectorComponent } from './selector/selector.component';
+import { OrganiserComponent } from './organiser/organiser.component';
+import { MomentPipe } from './shared/moment.pipe';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    SelectorComponent,
+    OrganiserComponent, 
+    MomentPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
